4d66ab12c68b4e9451b19279d49dbf68310e8d45
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-01 13:21:32 +0530

    added while loop, now it ask user to continue or stop

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,16 +1,23 @@
-number1=int(input("Enter the first Number:"))
-number2=int(input("Enter the second Number:"))
-operation=input("Enter '+' for addition, '-' for substraction, '*' for multiplication and '/' for division. ")
-result=0
-if operation=='+':
-    result=number1+number2
-    print(f"{number1} + {number2} = {result}")
-elif operation=='-':
-    result=number1-number2
-    print(f"{number1} - {number2} = {result}")
-elif operation=='*':
-    result=number1*number2
-    print(f"{number1} * {number2} = {result}")    
-else:
-    result=number1/number2
-    print(f"{number1} / {number2} = {result}")    
+# number1=int(input("Enter the first Number:"))
+# number2=int(input("Enter the second Number:"))
+# operation=input("Enter '+' for addition, '-' for substraction, '*' for multiplication and '/' for division. ")
+# result=0
+i=1
+while i==1:
+    number1=int(input("Enter the first Number:"))
+    number2=int(input("Enter the second Number:"))
+    operation=input("Enter '+' for addition, '-' for substraction, '*' for multiplication and '/' for division. ")
+    result=0
+    if operation=='+':
+        result=number1+number2
+        print(f"{number1} + {number2} = {result}")
+    elif operation=='-':
+        result=number1-number2
+        print(f"{number1} - {number2} = {result}")
+    elif operation=='*':
+        result=number1*number2
+        print(f"{number1} * {number2} = {result}")    
+    else:
+        result=number1/number2
+        print(f"{number1} / {number2} = {result}")    
+    i=int(input("Enter '1' to continue , Enter '0' to stop:" ))
