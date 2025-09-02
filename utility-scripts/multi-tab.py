e685f602707572e11efe9639011e1fa2d112d5c6
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-03 00:02:55 +0530

    6th script, multiplication tablet generator , using range() function, also writes in text file

--- utility-scripts/multi-tab.py
+++ utility-scripts/multi-tab.py
@@ -3,6 +3,10 @@ number=int(input("Enter the number:"))
 
 # for i in range(1,11):
 #     print(f"{number} * {i} : {number*i}")
-    
-for i in range(1, 11):
-    print(f"{number} x {i:2} = {number*i:3}")
+# open file in write mode
+with open(f"table_{number}.txt","w") as file:  
+    for i in range(1, 11):
+       line= f"{number} x {i:2} = {number*i:3}"
+       file.write(line) # write each line to file
+       print(line,end="")# Also print on screen
+        
