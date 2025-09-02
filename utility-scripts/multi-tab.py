e9621c06fe5b285655249dd5b6025f97b4276797
Vinod-Deshmukh <104995032+Vinod-Deshmukh@users.noreply.github.com>
2025-09-03 00:07:31 +0530

    Update multi-tab.py

--- utility-scripts/multi-tab.py
+++ utility-scripts/multi-tab.py
@@ -6,7 +6,7 @@ number=int(input("Enter the number:"))
 # open file in write mode
 with open(f"table_{number}.txt","w") as file:  
     for i in range(1, 11):
-       line= f"{number} x {i:2} = {number*i:3}"
+       line= f"{number} x {i:2} = {number*i:3}\n"
        file.write(line) # write each line to file
        print(line,end="")# Also print on screen
         
