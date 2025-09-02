d04232359fd02e2235e793b4e3216c3a5a88c4f7
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-02 14:17:44 +0530

    6th script, multiplication tablet generator , using range() function

--- utility-scripts/multi-tab.py
+++ utility-scripts/multi-tab.py
@@ -1,6 +1,8 @@
-# utility script 6
+# utility script 6 Mutliplication Tablet Generator. 
 number=int(input("Enter the number:"))
-# i=1
-for i in range(1,11):
-    print(f"{number} * {i} : {number*i}")
+
+# for i in range(1,11):
+#     print(f"{number} * {i} : {number*i}")
     
+for i in range(1, 11):
+    print(f"{number} x {i:2} = {number*i:3}")
