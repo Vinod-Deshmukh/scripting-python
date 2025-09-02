717fda322501ec45e5338972dc242ab132ee2934
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-02 13:54:36 +0530

    5th script, revers string, used slicing method to do so

--- reverse-string.py
+++ reverse-string.py
@@ -1,3 +1,4 @@
 word=input("Enter the word you want to reverse: ")
+# [::-1] start from the end and move backwords one step at a time.
 revers=word[::-1]
 print(f"you enter {word} and revers of this word is {revers}")
