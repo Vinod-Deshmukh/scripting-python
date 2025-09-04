f460fbbd9a10ae090211545e32d47c98f061924a
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-04 15:19:11 +0530

    8th script, countdown timer. uses in-built time module.and time.sleep() function.

---
+++ utility-scripts/countdown-timer.py
@@ -0,0 +1,10 @@
+# utility script 8 countdown timer
+# The time.sleep() function in Python is used to pause the execution 
+# of a program or a specific thread for a designated duration. 
+# This function is part of the built-in time module.
+import time
+
+seconds=int(input("Enter the time in seconds: "))
+# seconds : Number of seconds for which the code is required to be stopped.
+time.sleep(seconds)
+print(f"timer stopped the code for {seconds} seconds")
