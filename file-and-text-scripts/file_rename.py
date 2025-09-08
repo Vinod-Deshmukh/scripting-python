46113a16f41b85b6e87568e3e8d2ea3d7910fb8b
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-08 14:50:54 +0530

    script 12 , rename files in a folder.

--- file-and-text-scripts/file_rename.py
+++ file-and-text-scripts/file_rename.py
@@ -6,6 +6,16 @@ import os
 # os.mkdir(new_directory_name)
 # print(f'Directory {new_directory_name} creatd.')
 
-path='D:/august-2025/scripting-python/file-and-text-scripts/my_new_folder'
-dir=os.listdir(path)
-print(f'the list of files and directories {dir} \n')
+directory_path='D:/august-2025/scripting-python/file-and-text-scripts/my_new_folder'
+# file_name="my_new_file.txt"
+for i in range(5):
+    file_name=str(i)
+    full_file_path= os.path.join(directory_path,file_name)
+    with open(full_file_path,"w") as file:
+       file.write(f"This is the content of a new file-{file_name}")
+       print(f'file named {file_name} created successfully in {directory_path}     \n')
+# try: 
+# # for item_name in os.listdir(directory_path):
+#         full_path=os.path.join(directory_path,item_name)
+#         if os.path.isfile(full_path):
+#             print(f"File {item_name}")
