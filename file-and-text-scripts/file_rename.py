bd0572ee732f08cf80da4c425c3db13393622896
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-08 12:17:18 +0530

    script 12 , rename files in a folder.

--- file-and-text-scripts/file_rename.py
+++ file-and-text-scripts/file_rename.py
@@ -1,1 +1,11 @@
-# file and text script 12, Rename files in a folder.
+# file and text script 12, Rename files in a folder- add numbering to file names.
+import os
+# current_directory=os.getcwd()
+# print(f"current working directory:{current_directory}")
+# new_directory_name="my_new_folder"
+# os.mkdir(new_directory_name)
+# print(f'Directory {new_directory_name} creatd.')
+
+path='D:/august-2025/scripting-python/file-and-text-scripts/my_new_folder'
+dir=os.listdir(path)
+print(f'the list of files and directories {dir} \n')
