c76f03419bbccfa03805b1358a6fdbd2e0e8f81f
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-10 11:49:23 +0530

    script 13 , email exteactor . using regx. re.findall()method.

--- file-and-text-scripts/email_extractor.py
+++ file-and-text-scripts/email_extractor.py
@@ -1,4 +1,5 @@
 #Email Extractor , Script 13. From a given text, extract words with @.
+# username@domain.extension
 text="hello, this is vinod. my email is vinod@fun.com  " \
 "and my friends email is friend@fun.com , " \
 "my brother also has email vinod_bro@family.co.uk"
@@ -8,9 +9,26 @@ reg=re.findall(r"[A-Za-z0-9_%+-.]+"
                r"@[A-Za-z0-9.-]+"
                r"\.[A-Za-z]{2,5}",text)
 
-reg2 = re.findall(r"[A-Za-z0-9_%+-.]+"
-                 r"@[A-Za-z0-9.-]+"
-                 r"\.[A-Za-z]{2,5}",text)
+# 1. r"[A-Za-z0-9_%+-.]+"
 
+# This expression looks for a continuous sequence of characters consist 
+#  all capital alphabets defined by A-Z,
+#  lowercase alphabets a-z, all digits 0-9,
+#  and special characters such as _%+-. . 
+# The '+' is used to append the second regex to the first.
+
+# 2. r"@[A-Za-z0-9.-]+"
+
+# This expression looks for a continuous sequence of characters consist of
+#  all capital alphabets defined by A-Z, 
+# lowercase alphabets a-z, all digits 0-9, and 
+# special characters such as ._. 
+# The '+' is used to append the second regex to the first.
+
+# 3. r"\.[A-Za-z]{2,5}"
+
+# This expression looks for a continuous sequence of characters consist of
+#  all capital alphabets defined by A-Z, 
+# lowercase alphabets a-z such that the size of this continuous sequence is between 2-5 both inclusive.
 # printing all the emails found
 print(reg)
