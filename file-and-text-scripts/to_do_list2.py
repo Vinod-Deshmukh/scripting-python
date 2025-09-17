3ee3c27eeebce0b021d8ed7f95e424b2c612b75f
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-17 14:39:19 +0530

    script 15, python to_do_list

--- file-and-text-scripts/to_do_list2.py
+++ file-and-text-scripts/to_do_list2.py
@@ -3,3 +3,16 @@
 # to display use for loop. 
 # to add use appen() method.
 # to delete use pop() method.
+def display(tasks):
+    if not tasks:
+      print("\n ----Your To Do List is empty ----\n ")
+
+def main():
+   tasks=[]
+   print("\n -- Welcome to To Do List App -- \n")
+   print("Enter 1 to view List")
+
+if __name__=="__main__":
+    main()
+  
+  
