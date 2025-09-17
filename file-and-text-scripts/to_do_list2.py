2700bde56cfb28c7cb390398247cfab6239d2b5e
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-17 15:18:03 +0530

    script 15 , To Do List App

--- file-and-text-scripts/to_do_list2.py
+++ file-and-text-scripts/to_do_list2.py
@@ -6,15 +6,26 @@
 def display(tasks):
     if not tasks:
       print("\n ----Your To Do List is empty ----\n ")
+    else:
+        for i,task in enumerate(tasks):
+            print(f"{i+1}.{task}")  
+            
+def add(tasks):
+    new_task=input("Enter the Task: \n").strip()
+    tasks=tasks.append(new_task)
+    print(f" {new_task} added to list \n")
 
 def main():
-    tasks=[]
+    tasks=["Reading"]
     while True:
         print("\n -- Welcome to To Do List App -- \n")
         print("1. View Task \n")
+        print("2. Add Task \n")
         choice=input("Enter Your Choice: \n")
         if choice=='1':
             display(tasks)
+        elif choice=='2':
+            add(tasks)    
         else:
             break
 
