156696aa0ebabdf338b130b8c08bc0c08bcd9f4b
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-18 10:40:59 +0530

    added new path

--- practicle-automation-scripts/website_status_checker2.py
+++ practicle-automation-scripts/website_status_checker2.py
@@ -1,13 +1,20 @@
 # Script 16 Website Status Checker 2
-import requests
-from requests.exceptions import RequestException
-url=input("Enter the website URL (including http:// or https://): ")
-try:
-    response=requests.get(url)
-    if response.status_code==200:
-        print(f"{url} is UP!")
-    else:
-        print(f"{url} return status code {response.status_code} (may be DOWN or Have an Issue)) ")
-except RequestException as e:
-    print(f"{url} is DOWN!")
-    print(f"Error: {e}")
+import sys
+import os
+path=os.path.abspath(os.path.join(os.path.dirname(__file__),'..','file-and-text-scripts'))
+sys.path.append(path)
+print(path)
+import to_do_list as td
+td.main()
+# import requests
+# from requests.exceptions import RequestException
+# url=input("Enter the website URL (including http:// or https://): ")
+# try:
+#     response=requests.get(url)
+#     if response.status_code==200:
+#         print(f"{url} is UP!")
+#     else:
+#         print(f"{url} return status code {response.status_code} (may be DOWN or Have an Issue)) ")
+# except RequestException as e:
+#     print(f"{url} is DOWN!")
+#     print(f"Error: {e}")
