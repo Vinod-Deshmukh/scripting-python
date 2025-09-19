e98138722498a630016f1c115e7115a53244fc22
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-19 16:07:16 +0530

    script 18 updated added both api and random.choice

--- practicle-automation-scripts/joke_generator.py
+++ practicle-automation-scripts/joke_generator.py
@@ -3,20 +3,41 @@
 # Use random.choice(list)
 import requests
 import random
+# API url
 url="https://api.chucknorris.io/jokes/random"
-
-response=requests.get(url,timeout=10)
-print(f"Staus Code: {response.status_code} \n")
-data=response.json()
-# print("Pretty Response:")
-# print(json.dumps(data,indent=4))
-joke=data["value"]
-random_joke=["Chuck Norris once had sex with a dinosaur and then they became extinct."
+# Local backup jokes
+local_jokes=["Chuck Norris once had sex with a dinosaur and then they became extinct."
 ,"Chuck Norris is so American, he eats tyranny and shits apple pie."
 ,"Chuck Norris can access the DB from the UI."
 ,"Chuck Norris can rear-end his own car."
 ,"Chuck Norris taught Prometheus how to wipe his ass."
 ]
-joke_list=random.choice(random_joke)
-print(f"Joke:{joke}")
-print(f"Joke:{joke_list}")
+def get_api_jokes():
+    """Fetch Jokes from API,return None if fails."""
+    try:
+        response=requests.get(url,timeout=5)
+        # raises error if not 200
+        response.raise_for_status()
+        data=response.json()
+        return data.get("value",None)
+    except Exception:
+        return None
+def get_random_joke():
+    """Randomly chooses API jokes or local jokes."""
+    # 50% chance.
+    if random.choice([True,False]):
+        joke=get_api_jokes()
+        if joke:
+            return joke
+        # Fallback to local jokes
+        return random.choice(local_jokes)
+# Main program
+if __name__=="__main__":
+    try:
+        count=int(input("How many jokes do you want?"))
+    except ValueError:
+        # default to 1 if input is invalid.
+        count=1
+    print("\n Here are your jokes:\n")
+    for i in range(count):
+        print(f"{i+1}.{get_random_joke()}")
