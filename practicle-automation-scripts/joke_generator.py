7345722a6eb95df20a39ad4c5f8c655fd6291f09
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-19 15:43:23 +0530

    script 18 updated added both api and random.choice

--- practicle-automation-scripts/joke_generator.py
+++ practicle-automation-scripts/joke_generator.py
@@ -2,7 +2,7 @@
 # store Quotes in a List 
 # Use random.choice(list)
 import requests
-import json
+import random
 url="https://api.chucknorris.io/jokes/random"
 
 response=requests.get(url,timeout=10)
@@ -11,4 +11,12 @@ data=response.json()
 # print("Pretty Response:")
 # print(json.dumps(data,indent=4))
 joke=data["value"]
-print(f"Joke: {joke}")
+random_joke=["Chuck Norris once had sex with a dinosaur and then they became extinct."
+,"Chuck Norris is so American, he eats tyranny and shits apple pie."
+,"Chuck Norris can access the DB from the UI."
+,"Chuck Norris can rear-end his own car."
+,"Chuck Norris taught Prometheus how to wipe his ass."
+]
+joke_list=random.choice(random_joke)
+print(f"Joke:{joke}")
+print(f"Joke:{joke_list}")
