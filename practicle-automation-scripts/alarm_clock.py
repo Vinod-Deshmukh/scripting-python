796a49858eeca249bcf5dc433100040e64ac3780
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-20 18:22:06 +0530

    script 19. Simple alarm clock. Using Datetime , time

---
+++ practicle-automation-scripts/alarm_clock.py
@@ -0,0 +1,11 @@
+# Script 19 Simple Alarm Clock – Alert at user-given time.
+# import datetime, time.
+# Input alarm time (HH:MM).
+# While loop → check current time.
+
+from datetime import datetime
+from datetime import date
+current_datetime=datetime.now()
+current_date=date.today()
+print(f"{current_datetime}")
+print(f"{current_date}")
