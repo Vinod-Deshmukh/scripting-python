38b2429325a5b83d5cde8a8823abb0d0bd6d6e71
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-09-22 19:25:01 +0530

    ValurError: when function receives value of a wrong type

--- practicle-automation-scripts/alarm_clock2.py
+++ practicle-automation-scripts/alarm_clock2.py
@@ -6,16 +6,20 @@ import os
 def set_alarm():
     while True:
         try:
-            alram_time_str=input("Enter the time in HH:MM:SS Format")
+            alram_time_str=input("Enter the time in HH:MM:SS Format \n")
             # Validate Input Format
-            if(alram_time_str!=8 or alram_time_str[2]!=":" or alram_time_str[5]!= ":"):
+            if(len(alram_time_str)!=8 or alram_time_str[2]!=":" or alram_time_str[5]!= ":"):
                 raise ValueError("Invalid format.Please use HH:MM:SS format")
-            alarm_hour=alram_time_str[0:2]
-            alarm_minutes=alram_time_str[3:5]
-            alarm_seconds=alram_time_str[6:8]
-            if not(0<=alarm_hour<=23 and 0<=alarm_minutes<=59 and 0<=alarm_seconds<=59):
+            
+            alarm_hour=int(alram_time_str[0:2])
+            alarm_minutes=int(alram_time_str[3:5])
+            alarm_seconds=int(alram_time_str[6:8])
+
+            if not(0 <= alarm_hour <= 23 and 0 <= alarm_minutes <= 59 and 0 <= alarm_seconds <= 59):
                 raise ValueError("Invalid time values. Hour(0-23).Minutes(0-59).Seconds(0-59)")
             # Breaks the loop if input is valid
             break
         except ValueError as e:
             print(f"Error {e}.Please try again.")
+    print(f"alarm set for: {alram_time_str}")
+set_alarm()
