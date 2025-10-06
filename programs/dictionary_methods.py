567e15b51d04f52faf314d9845ba51ee145c098f
Vinod-Deshmukh <vinod.256.14@gmail.com>
2025-10-06 09:51:55 +0530

    learned 10 important dictionary methods

--- programs/dictionary_methods.py
+++ programs/dictionary_methods.py
@@ -3,3 +3,31 @@ data={'a':1,'b':2,'c':3,'d':4}
 #get() safely get value
 print(data.get('b'))
 print(data.get('x'))
+#keys() get all keys
+dict_keys=data.keys()
+print(dict_keys)
+#values() get all values
+dict_vlaues=data.values()
+print(dict_vlaues)
+#items() get key value pair
+dict_items=data.items()
+print(dict_items)
+#update() merge another dictionary
+data.update({'e':5})
+print(data)
+#pop() remove by key
+data.pop('b')
+print(data)
+#popitem() remove last inserted pair
+data.popitem()
+print(data)
+#clear() remove all the items
+# data.clear()
+# print(data)
+# setdefalut() get values or set if not present
+data.setdefault('f',0)
+#adds 'f':0 if not present
+print(data)
+#fromkeys() create dictionary from keys
+data_new=dict.fromkeys(['x','y'],0)
+print(data_new)
